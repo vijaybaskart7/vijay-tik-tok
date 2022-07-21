fecc5f3e483e79375d98b1d6c7ff4558f07c089a
vijybaskar <vijaybaskar.t7@gmail.com>
2022-07-21 22:27:11 +0530

    changing build config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors:true
+  },
   reactStrictMode: true,
   images: {
     domains: ['unsplash.com', 'picsum.photos', 'lh3.googleusercontent.com'],
